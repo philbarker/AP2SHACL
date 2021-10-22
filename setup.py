f92e138549ac1386a1f6be2b375d7cb0b43b96df
Phil Barker <phil.barker@pjjk.co.uk>
2021-10-22 10:49:05 +0100

    minor version increment

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='AP2SHACL',
-      version='0.1',
+      version='0.1.1',
       description='Application profile to SHACL exporter',
       author='Phil Barker',
       author_email='phil@pjjk.net',
