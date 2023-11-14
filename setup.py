71392cdf61d9749ac1d1a148d8fad03667fa005c
Phil Barker <phil.barker@pjjk.co.uk>
2023-11-14 11:56:27 +0000

    incr version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name="AP2SHACL",
-    version="0.4.0",
+    version="0.4.1",
     description="Application profile to SHACL exporter",
     author="Phil Barker",
     author_email="phil@pjjk.net",
