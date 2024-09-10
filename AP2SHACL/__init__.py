bf90997062470ded0b1ebed0bd5aca8cce93cc1e
Phil Barker <phil.barker@pjjk.co.uk>
2024-09-10 15:09:45 +0100

    Moved

--- AP2SHACL/__init__.py
+++
@@ -1,8 +0,0 @@
-from AP import AP, StatementTemplate, ShapeInfo, read_shapeInfoDict
-from .ap2shaclConverter import (
-    AP2SHACLConverter,
-    make_property_shape_name,
-    list2RDFList,
-    str2URIRef,
-    convert_nodeKind,
-)
--- main.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-from AP import AP, StatementTemplate
-from AP2SHACL import AP2SHACLConverter, make_property_shape_id, list2RDFList
-
-if __name__ == "__main__":
-    print("Will convert an application profile AP into SHACL.")
-    print("Intended to be used in a program that loads the AP.")
-    print("No command line use.")
